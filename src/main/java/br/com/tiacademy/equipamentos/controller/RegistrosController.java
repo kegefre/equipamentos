d8fa6a847822226e1ed609e9001842747241bc7f
kellisgf <kellisgf@gmail.com>
2022-07-24 22:09:59 -0300

    Atualização

--- src/main/java/br/com/tiacademy/equipamentos/controller/RegistrosController.java
+++ src/main/java/br/com/tiacademy/equipamentos/controller/RegistrosController.java
@@ -1,13 +1,30 @@
 package br.com.tiacademy.equipamentos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tiacademy.equipamentos.core.crud.CrudController;
 import br.com.tiacademy.equipamentos.domain.Registros;
+import br.com.tiacademy.equipamentos.service.RegistrosService;
 
 @RestController
 @RequestMapping("/registros")
 public class RegistrosController extends CrudController<Registros, Long>{
 	
+	@Autowired
+	RegistrosService service;
+	
+	@PostMapping("/saida")
+	public Registros regSaida(@RequestBody Registros registro) {
+		return service.regSaida(registro);
+	}
+	
+	@PutMapping("/entrada")
+	public Registros regEntrada(@RequestBody Registros registro) {
+		return service.regEntrada(registro);
+	}
 }
--- src/main/java/br/com/tiacademy/equipamentos/service/RegistrosService.java
+++ src/main/java/br/com/tiacademy/equipamentos/service/RegistrosService.java
@@ -1,12 +1,17 @@
 package br.com.tiacademy.equipamentos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.tiacademy.equipamentos.core.crud.CrudService;
+import br.com.tiacademy.equipamentos.domain.Equipamentos;
 import br.com.tiacademy.equipamentos.domain.Registros;
 
 @Service
 public class RegistrosService extends CrudService<Registros, Long> {
+	@Autowired
+	private EquipamentosService serviceEquip;
+	
 	@Override
 	protected Registros editarEntidade(Registros recuperado, Registros entidade) {
 		recuperado.setEquip_id(entidade.getEquip_id());
@@ -16,5 +21,23 @@ public class RegistrosService extends CrudService<Registros, Long> {
 		recuperado.setSituacao(entidade.getSituacao());
 		return recuperado;
 	}
+	
+	public Registros regSaida(Registros registro) {
+		Registros salvo=this.criar(registro);
+		Equipamentos equip = serviceEquip.porId(registro.getEquip_id());
+		equip.setDisponiveis(equip.getDisponiveis()-1);
+		serviceEquip.editar(registro.getEquip_id(),equip);
+		return salvo;
+	}
+	
+	public Registros regEntrada(Registros registro) {
+		registro.setSituacao(2);
+		this.editar(registro.getId(), registro);
+		Equipamentos equip = serviceEquip.porId(registro.getEquip_id());
+		equip.setDisponiveis(equip.getDisponiveis()+1);
+		serviceEquip.editar(registro.getEquip_id(),equip);
+		return registro;
+	}
+	
 	
 }
